17987c2902d535b2ab8a5822922c07c9015d603c
yuwotmate <kaleb.nim@gmail.com>
2022-12-20 02:08:15 +0800

    GOT ALOT OF BUGS AHHHHH

--- RAD_neRF/app.py
+++ RAD_neRF/app.py
@@ -1,10 +1,12 @@
 from flask import Flask, request
 from flask_cors import CORS
-
+import os
 import subprocess
 import wave
 import io
 import requests
+
+from base64 import b64encode
 app = Flask(__name__)
 CORS(app)
 # @markdown ####**Settings:**
@@ -21,27 +23,79 @@ Pose_end = 100  # @param {type: 'integer'}
 #     run_extract = subprocess.run(['python', 'nerf/asr.py', '--wav', 'data/' + Aud, '--save_feats'])
 #     %run nerf/asr.py --wav data/{Aud} --save_feats
 
-# # print(f'[INFO] use audio file: {Aud}')
+# # print(f'[INFO] use audio file: {Aud}')'
+
 
 @app.route('/get-blob-data', methods=['POST'])
 def get_blob_data():
     print(f"went into get-blob-data")
-    print(request)
-    blob_data = request.data
-    print(f"blob_data==> {blob_data}")
+    data = request.files
+    print(f"data['audioBlob']==>", data['audioBlob'])
+    data['audioBlob'].save('data/nvp_HY.wav')
+
+
     # # do something with the blob data here
-    # with wave.open(io.BytesIO(data), "rb") as f:
-    #     # Step 3: Use the write method to write the wave file data to a new .wav file
-    #     with wave.open("recording.wav", "wb") as out:
-    #         out.setparams(f.getparams())
-    #         out.writeframes(f.readframes(f.getnframes()))
+
+    # Code to extract audio features
+    """
+    Takes in .wav file, performs Audio-spatial Decomposition and returns .npy file of audio features
+    """
     try:
-        runInference = subprocess.run(['python', 'test.py', '-O', '--torso', '--pose', 'data/pose.json', '--data_range', '0', '100', '--ckpt', 'pretrained/model.pth', '--aud', 'data/engm_eo.npy', '--bg_img', 'data/bg.jpg'], check=True, stderr=subprocess.PIPE)
+        run_extract = subprocess.run(['python', 'nerf/asr.py', '--wav', 'data/nvp.wav', '--save_feats'], check=True, stderr=subprocess.PIPE)
     except subprocess.CalledProcessError as e:
+        print(f'Extract audio features failed:')
         print(e.stderr.decode())
 
-    print(f"runInference==> {runInference}")
-    return 'Success'
+
+    # # # Code to run inference CUDA broke this AAAAAAAAAAAAAAAAAAAAAAAAAAAA
+    # """
+    # Takes in .npy file of audio features, runs inference and returns .mp4 file of face video (without audio)
+    # """
+    # try:
+    #     runInference = subprocess.run(['python', 'test.py', '-O', '--torso', '--pose', 'data/pose.json', '--data_range', '0', '100', '--ckpt', 'pretrained/model.pth', '--aud', 'data/nvp_eo.npy', '--bg_img', 'data/bg.jpg'], check=True, stderr=subprocess.PIPE)
+    # except subprocess.CalledProcessError as e:
+    #     print(f'Run inference failed:')
+    #     print(e.stderr.decode())
+    # print(f"runInference==> {runInference}")
+    
+    
+    Video = 'trail/results/ngp_ep0059.mp4' # Hard coded for now
+    Video_aud = Video.replace('.mp4', '_aud.mp4')
+
+    # Concat audio with video
+    """Returns"""
+    try:
+        concat_audio_command = subprocess.run(['ffmpeg', '-y', '-i', Video, '-i', 'data/nvp.wav', '-c:v', 'copy', '-c:a', 'aac', Video_aud], check=True, stderr=subprocess.PIPE)
+    except subprocess.CalledProcessError as e:
+        print(f'Concat audio failed:')
+        print(e.stderr.decode())
+
+    # Convert video to base64
+    video_file = open(Video_aud, "r+b").read()
+    video_url = f"data:video/mp4;base64,{b64encode(video_file).decode()}"
+
+    return video_url
+
+@app.route('/test-return-video-data', methods=['POST'])
+def get_audio_data():
+    print(f"went into get_audio_data")
+    
+    Video = 'trail/results/ngp_ep0059.mp4' # Hard coded for now
+    Video_aud = Video.replace('.mp4', '_aud.mp4')
+
+    # Concat audio with video
+    """Returns"""
+    try:
+        concat_audio_command = subprocess.run(['ffmpeg', '-y', '-i', Video, '-i', 'data/nvp.wav', '-c:v', 'copy', '-c:a', 'aac', Video_aud], check=True, stderr=subprocess.PIPE)
+    except subprocess.CalledProcessError as e:
+        print(f'Concat audio failed:')
+        print(e.stderr.decode())
+
+    # Convert video to base64
+    video_file = open(Video_aud, "r+b").read()
+    video_url = f"data:video/mp4;base64,{b64encode(video_file).decode()}"
+
+    return video_url
 
 
 @app.route('/')
--- RAD_neRF/main.py
+++ RAD_neRF/main.py
@@ -1,2 +1,6 @@
 import os
 print(os.environ.get('CUDA_PATH'))
+# Check if .npy file exists in data folder
+
+if os.path.exists('data/*.npy'):
+    print("File exists")
