84e014719be70ee02903d2d73c2951650530a675
Joel <2b11.2112729.joelpoah@gmail.com>
2022-12-19 19:53:23 +0800

    working multiple sockets

--- server.js
+++ server.js
@@ -4,8 +4,11 @@ const server = require("http").Server(app);
 const io = require("socket.io")(server);
 const PORT = 8081;
 
+// Create a socket namespace for chat messages
+
 app.set("view engine", "ejs");
 app.use(express.static("public"));
+
 
 app.get("/", (req, res) => {
   res.render("index");
@@ -19,11 +22,12 @@ app.get("/ws/incoming", (req, res) => {
   res.render("incoming_call");
 });
 
-app.get("/ws/call/:room", (req, res) => {
-  res.render("call", { roomId: req.params.room });
-});
+app.get("/ws/call/:room",(req,res)=>{
+  res.render("call",{ roomId: req.params.room})
+  });
 
 io.on("connection", (socket) => {
+  console.log("New user connected");
   socket.emit("clientid", socket.id);
   socket.on("join-room", (roomId, userId) => {
     socket.join(roomId);
@@ -37,6 +41,12 @@ io.on("connection", (socket) => {
     io.to(roomId).emit("message", { user: socket.id, message });
   });
 });
+
+
+
+
+
+
 
 server.listen(process.env.PORT || PORT);
 console.log(
